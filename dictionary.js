d0f97e9ba5452d0318995453df73f50eca0a4189
esradenisa <esra.nechifor@yahoo.com>
2024-09-07 16:01:33 +0300

    dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -4,8 +4,9 @@ function addWord() {
     const word = document.getElementById('addWord').value.toLowerCase();
     if (word && !dictionary.includes(word)) {
         dictionary.push(word);
+        document.getElementById('message').innerText = 'The word was saved in the dictionary.';
     } else if (dictionary.includes(word)) {
-        document.getElementById('message').innerText = 'Cuvantul a fost adaugat in dictionar.';
+        document.getElementById('message').innerText = 'The word is already saved in the dictionary.';
     }
     document.getElementById('addWord').value = '';
 }
@@ -13,9 +14,9 @@ function addWord() {
 function searchWord() {
     const word = document.getElementById('searchWord').value.toLowerCase();
     if (dictionary.includes(word)) {
-        document.getElementById('message').innerText = 'Cuvantul se afla in dictionar.';
+        document.getElementById('message').innerText = 'We found the word in the dictionary.';
     } else {
-        document.getElementById('message').innerText = 'Cuvantul nu se afla in dictionar.';
+        document.getElementById('message').innerText = 'We did not find the word in the dictionary.';
     }
     document.getElementById('searchWord').value = '';
 }
